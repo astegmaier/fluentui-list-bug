a3645539b0797d4ac43c7cf2151f6fac38ba76f1
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2020-07-15 22:00:32 -0400

    Create repro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { List, Provider, teamsTheme } from "@fluentui/react-northstar";
 
-const App = () => <div>Hello World!</div>
+const items = [
+  {
+    key: "irving",
+    header: "Irving Kuhic",
+    content: "Program the sensor to the SAS alarm through the haptic SQL card!",
+  },
+  {
+    key: "skyler",
+    header: "Skyler Parks",
+    content:
+      "Use the online FTP application to input the multi-byte application!",
+  },
+  {
+    key: "dante",
+    header: "Dante Schneider",
+    content: "The GB pixel is down, navigate the virtual interface!",
+  },
+];
+
+const App = () => (
+  <Provider theme={teamsTheme}>
+    <List selectable defaultSelectedIndex={0} items={items} />
+  </Provider>
+);
 
 export default App;
